cbc8ea8e338278a06606876593943cdd20962ceb
dexter <dexter.lente18@gmail.com>
2025-01-15 06:42:52 +0800

    modify url

--- novels/api/urls.py
+++ novels/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-     path('novels/', views.PaginatedNovelsProtobufView.as_view()),
+     path('novels', views.PaginatedNovelsProtobufView.as_view()),
      path('novel-details/<int:novel_id>/', views.NovelDetailsView.as_view()),
      path('novels/single/', views.FilterNovelsBySingleGenreView.as_view()),
      path('chapters/<int:novel_id>', views.PaginatedChaptersListView.as_view()),
