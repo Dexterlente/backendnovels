4236f7fb52a20e19a9cb64edbfb2e3e33a6ae9b9
dexter <dexter.lente18@gmail.com>
2025-01-15 20:28:21 +0800

    added admin

--- novels/api/admin.py
+++ novels/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import CustomUser, Novels, Chapters
 # Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Novels)
+admin.site.register(Chapters)
